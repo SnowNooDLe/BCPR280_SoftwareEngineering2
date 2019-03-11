239352bbb5766ab58c657b8ca21e77153f287d13
SnowNooDLe <sdw123123@gmail.com>
2019-03-11 22:35:04 +1300

    make it as external js

--- Session04/PSP 01/ClassWork01.js
+++ Session04/PSP 01/ClassWork01.js
@@ -1,14 +1,35 @@
-function createTable(output) {
-    var i=0, rowEl=null,
+function createTable() {
+    // displaying lists of numbers and sum
+    var i=0, j=0, rowEl=null,
     tableEl = document.createElement("table");
-    var lines = document.getElementById(output).innerHTML.split('\r\n');
-    console.log("ERROR");
-    console.log(lines);
-    // create 10 table rows, each with two cells
-    for (i=1; i <= 10; i++) {
+    var lines = document.getElementById("output").innerHTML.split('\r\n');
+    var sum = 0;
+    var stddiv = 0;
+    var calculated_stddiv = 0;
+    for (i=0; i < 10; i++) {
         rowEl = tableEl.insertRow();  // DOM method for creating table rows
-        rowEl.insertCell().textContent = "table cell "+ i +"-1" ;      
-        rowEl.insertCell().textContent = "table cell "+ i +"-2" ;      
+        rowEl.insertCell().textContent = i + 1;
+        rowEl.insertCell().textContent = lines[i];
+        sum += parseFloat(lines[i]);
     }
-    document.body.appendChild( tableEl);
+    // sum part
+    rowEl = tableEl.insertRow();
+    rowEl.insertCell().textContent = "SUM";
+    rowEl.insertCell().textContent = sum;
+    sumArray.push(sum);
+
+    // standard deviation
+    
+    for (j =0; j < 10; j++){
+        stddiv += Math.pow((parseFloat(lines[j] - sum)),2);
+    }
+    // console.log(stddiv);
+    calculated_stddiv = Math.sqrt(stddiv/9);
+    // console.log(calculated_stddiv);
+
+    rowEl = tableEl.insertRow();
+    rowEl.insertCell().textContent = "SD";
+    // print only upto 4decimal points
+    rowEl.insertCell().textContent = calculated_stddiv.toFixed(4);
+    test.appendChild(tableEl);
 }
