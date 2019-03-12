778d19e31e467c4684eeb438d9d98c152b71496d
SnowNooDLe <sdw123123@gmail.com>
2019-03-12 22:04:02 +1300

    Adding some comments

--- Session04/PSP 01/ClassWork01.js
+++ Session04/PSP 01/ClassWork01.js
@@ -1,11 +1,23 @@
+//      BCPR280 Software Engineering 2
+//      Program to calculate the Mean and Standard Deviation
+//      by parsing data from txt file by user
+//      Author: Tom Son 
+    
+//      Main JavaScript file
+
+//      github address: github.com/SnowNooDLe
+
 function createTable() {
     // displaying lists of numbers and sum
     var i=0, j=0, rowEl=null,
     tableEl = document.createElement("table");
+    // will be lines of numbers in textarea called 'output' and read their data
+    // and store to lines line by line
     var lines = document.getElementById("output").innerHTML.split('\r\n');
     var sum = 0;
     var stddiv = 0;
     var calculated_stddiv = 0;
+    
     for (i=0; i < 10; i++) {
         rowEl = tableEl.insertRow();  // DOM method for creating table rows
         rowEl.insertCell().textContent = i + 1;
