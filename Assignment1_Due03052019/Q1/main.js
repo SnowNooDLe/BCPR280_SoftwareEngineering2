99c80c6a32cb541462a89e7f0ceccb563da55a9a
SnowNooDLe <sdw123123@gmail.com>
2019-03-30 00:13:38 +1300

    almost there, need to figure out M part from MVC

--- Assignment1_Due03052019/Q1/main.js
+++ Assignment1_Due03052019/Q1/main.js
@@ -1,11 +1,39 @@
+
+// CONTROLLER
 var Q1 = new Vue({
     el:'#appendixTwoOne',
     data: {
         randomNum: Math.floor(Math.random() * 100),
         count: 0,
         txtInput: '',
-        result: '',
-        message: 'am i getting'
+        result: ''
+    },
+    methods: {
+        compareNumbers: function () {
+            this.count++
+            console.log("User input is " + this.txtInput)
+            console.log("Generated number is " + this.randomNum)
+            if (this.txtInput < this.randomNum){
+                this.result = "Try Higher"
+            } else if (this.txtInput > this.randomNum){
+                this.result = "Try Lower"
+            } else if (this.txtInput == this.randomNum){
+                this.result = `You got it in ${this.count} trials`
+            }
+        }
+    }
+});
+
+
+
+// CONTROLLER
+var Q2 = new Vue({
+    el:'#appendixTwoTwo',
+    data: {
+        randomNum: Math.floor(Math.random() * 100),
+        count: 0,
+        txtInput: '',
+        result: ''
     },
     methods: {
         compareNumbers: function () {
