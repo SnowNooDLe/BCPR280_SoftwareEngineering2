81c31a99e597f4f1a0afd4e53b6a18d416d8141b
SnowNooDLe <sdw123123@gmail.com>
2019-03-30 01:11:29 +1300

    Q1 seems okay in MVC structure

--- Assignment1_Due03052019/Q1/main.js
+++ Assignment1_Due03052019/Q1/main.js
@@ -1,52 +1,82 @@
+// Model
+class Game {
+    
+    count = 0
+    generatedNum = Math.floor(Math.random() * 100)
+    constructor () {
+    }
+
+    generateNumberByGame(){
+        return this.generatedNum
+    }
+    
+    compareNumbers(userInput, randomNum){
+        if (userInput > 99 || userInput < 0){
+            return "Number is between 0 and 99"
+        }
+        this.count++
+        if (userInput < randomNum){
+            return "Try Higher"
+        } else if (userInput > randomNum){
+            return "Try Lower"
+        } else if (userInput == randomNum){
+            return `You got it in ${this.count} trials!`
+        }  else{
+            return "Something is not right !"
+        }
+    }
+
+    getCountValue(){
+        return this.count
+    }
+
+}
+
 
 // CONTROLLER
+// Q1 seems okay
 var Q1 = new Vue({
     el:'#appendixTwoOne',
     data: {
-        randomNum: Math.floor(Math.random() * 100),
+        game: new Game(),
+        randomNum: 0,
         count: 0,
         txtInput: '',
         result: ''
     },
     methods: {
         compareNumbers: function () {
-            this.count++
+            this.randomNum = this.game.generateNumberByGame()
             console.log("User input is " + this.txtInput)
             console.log("Generated number is " + this.randomNum)
-            if (this.txtInput < this.randomNum){
-                this.result = "Try Higher"
-            } else if (this.txtInput > this.randomNum){
-                this.result = "Try Lower"
-            } else if (this.txtInput == this.randomNum){
-                this.result = `You got it in ${this.count} trials`
-            }
+            this.result = this.game.compareNumbers(this.txtInput, this.randomNum)
+            this.count = this.game.getCountValue()
         }
     }
 });
 
-
-
 // CONTROLLER
+// working on Q2
 var Q2 = new Vue({
     el:'#appendixTwoTwo',
     data: {
-        randomNum: Math.floor(Math.random() * 100),
+        game: new Game(),
+        randomNum: 0,
         count: 0,
         txtInput: '',
         result: ''
     },
     methods: {
         compareNumbers: function () {
-            this.count++
+            this.randomNum = this.game.generateNumberByGame()
             console.log("User input is " + this.txtInput)
             console.log("Generated number is " + this.randomNum)
-            if (this.txtInput < this.randomNum){
-                this.result = "Try Higher"
-            } else if (this.txtInput > this.randomNum){
-                this.result = "Try Lower"
-            } else if (this.txtInput == this.randomNum){
-                this.result = `You got it in ${this.count} trials`
-            }
+            this.result = this.game.compareNumbers(this.txtInput, this.randomNum)
+            this.count = this.game.getCountValue()
         }
     }
 });
+
+
+
+
