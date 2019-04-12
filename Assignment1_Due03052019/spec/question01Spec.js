917271e5617a476b705d2095d0d8b3328a99be3e
SnowNooDLe <sdw123123@gmail.com>
2019-04-12 16:22:44 +1200

    update

--- Assignment1_Due03052019/spec/question01Spec.js
+++ Assignment1_Due03052019/spec/question01Spec.js
@@ -1,4 +1,69 @@
 /* global describe  */
-describe('Question One', function () {
-  describe('Draw a class diagram', function () {})
+describe('Game One', () => {
+  // setup game area
+  describe('Create Game One', () => {
+    let gameSetup
+    beforeEach(() =>{
+      gameSetup = new Game()
+    })
+
+    // checking whether its generating random number 
+    it('should return a random number', () => {
+      expect(gameSetup.generateNumberByGame()).toMatch(/\d{1,}/)
+    })
+
+    // Checking whether its initial count value is 0
+    it('Initial count value must be 0', () => {
+      expect(gameSetup.getCountValue()).toMatch(0)
+    })
+  })
+  describe('Testing Game One', () => {
+    let gameTwoOne
+    beforeEach(() => {
+      gameTwoOne = new Q1()
+      gameTwoOne.generatedNum = 30
+    })
+    // Test #1
+    describe('Test #1 "To determine if the program responses "Try Lower" ', () => {
+      var userInput = 50
+      it('Should print "Try Lower" ', () => {
+        expect(gameTwoOne.compareNumbersQ1(userInput, gameTwoOne.generatedNum)).toBe('Try Lower')
+      })
+    })
+    // Test #2
+    describe('Test #2 "To determine if the program responses "Try Higher" ', () => {
+      var userInput = 20
+      it('Should print "Try Higher" ', () => {
+        expect(gameTwoOne.compareNumbersQ1(userInput, gameTwoOne.generatedNum)).toBe('Try Higher')
+      })
+    })
+    // Test #3
+    describe('Test #3 "To determine if the program responses "You got it in n trials!" ', () => {
+      var userInput = 30
+      it('Should print "You got it in 1 trials!" ', () => {
+        expect(gameTwoOne.compareNumbersQ1(userInput, gameTwoOne.generatedNum)).toBe('You got it in 1 trials!')
+      })
+    })
+    // Test #4
+    describe('Test #4 "To determine if the program responses "Invalid Try, please put integer value" ', () => {
+      var userInput = ''
+      it('Should print "Invalid Try, please put integer value in to guess" ', () => {
+        expect(gameTwoOne.compareNumbersQ1(userInput, gameTwoOne.generatedNum)).toBe('Invalid Try, please put integer value in to guess')
+      })
+    })
+    // Test #5
+    describe('Test #5 "To determin if the program tells user that randomly generated number is between 0 and 99 ', () => {
+      var userInput = 105
+      it('Should print "Randomly generated number is between 0 and 99" ', () => {
+        expect(gameTwoOne.compareNumbersQ1(userInput, gameTwoOne.generatedNum)).toBe('Randomly generated number is between 0 and 99')
+      })
+    })
+    // Test #6
+    describe('Test #2 "To determine if the program responses "Invalid Try, please put integer value" ', () => {
+      var userInput = 'abcd'
+      it('Should print "Invalid Try, please put integer value in to guess" ', () => {
+        expect(gameTwoOne.compareNumbersQ1(userInput, gameTwoOne.generatedNum)).toBe('Invalid Try, please put integer value in to guess')
+      })
+    })
+  })
 })
--- Assignment1_Due03052019/spec/question02Spec.js
+++ Assignment1_Due03052019/spec/question02Spec.js
@@ -1,42 +1,83 @@
-/* globals describe beforeEach Controller it expect  */
-describe('Question Two', () => {
-  describe('adding the four CatOwners', () => {
-    let theCattery
+/* global describe  */
+describe('Game Two', () => {
+  // setup game area
+  describe('Create Game Two', () => {
+    let gameSetup
+    beforeEach(() =>{
+      gameSetup = new Game()
+    })
+
+    // checking whether its generating random number 
+    it('should return a random number', () => {
+      expect(gameSetup.generateNumberByGame()).toMatch(/\d{1,}/)
+    })
+
+    // Checking whether its initial count value is 0
+    it('Initial count value must be 0', () => {
+      expect(gameSetup.getCountValue()).toMatch(0)
+    })
+  })
+  describe('Testing Game One', () => {
+    let gameTwoTest
     beforeEach(() => {
-      theCattery = Controller.setup()
+      gameTwoTest = new Q2()
+      gameTwoTest.generatedNum = 30
     })
-    it('should have added 4 CatOwners', () => {
-      expect(theCattery.allMyCatOwners.length).toBe(4)
+    // Test #1
+    describe('Test #1 "To determine if the program responses "COLD" ', () => {
+      var userInput = 80
+      it('Should print "COLD" ', () => {
+        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('COLD')
+      })
     })
-    it('should have correctly set details for each CatOwner', () => {
-      theCattery.sortCatOwners()
-      let aCatOwner = theCattery.allMyCatOwners[0]
-      expect(aCatOwner).toBeDefined()
-      expect(aCatOwner.id).toBe('4')
-      expect(aCatOwner.firstName).toBe('Temepara')
-      expect(aCatOwner.lastName).toBe('Williams')
-      expect(aCatOwner.amountOwed).toBe(12.56)
-
-      aCatOwner = theCattery.allMyCatOwners[1]
-      expect(aCatOwner).toBeDefined()
-      expect(aCatOwner.id).toBe('7')
-      expect(aCatOwner.firstName).toBe('Ruth')
-      expect(aCatOwner.lastName).toBe('George')
-      expect(aCatOwner.amountOwed).toBe(12.34)
-
-      aCatOwner = theCattery.allMyCatOwners[2]
-      expect(aCatOwner).toBeDefined()
-      expect(aCatOwner.id).toBe('23')
-      expect(aCatOwner.firstName).toBe('Casey')
-      expect(aCatOwner.lastName).toBe('van Dyke')
-      expect(aCatOwner.amountOwed).toBe(34.56)
-
-      aCatOwner = theCattery.allMyCatOwners[3]
-      expect(aCatOwner).toBeDefined()
-      expect(aCatOwner.id).toBe('165')
-      expect(aCatOwner.firstName).toBe('Irene')
-      expect(aCatOwner.lastName).toBe('Aitkin')
-      expect(aCatOwner.amountOwed).toBe(56.12)
+    // Test #2
+    describe('Test #2 "To determine if the program responses "COOL" ', () => {
+      var userInput = 60
+      it('Should print "COOL" ', () => {
+        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('COOL')
+      })
+    })
+    // Test #3
+    describe('Test #3 "To determine if the program responses "WARM" ', () => {
+      var userInput = 45
+      it('Should print "WARM" ', () => {
+        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('WARM')
+      })
+    })
+    // Test #4
+    describe('Test #4 "To determine if the program responses "HOT" ', () => {
+      var userInput = 31
+      it('Should print "HOT" ', () => {
+        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('HOT')
+      })
+    })
+    // Test #5
+    describe('Test #5 "To determine if the program responses "You got it in n trials!" ', () => {
+      var userInput = 30
+      it('Should print "You got it in 1 trials!" ', () => {
+        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('You got it in 1 trials!')
+      })
+    })
+    // Test #6
+    describe('Test #6 "To determine if the program responses "Invalid Try, please put integer value" ', () => {
+      var userInput = ''
+      it('Should print "Invalid Try, please put integer value in to guess" ', () => {
+        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('Invalid Try, please put integer value in to guess')
+      })
+    })
+    // Test #7
+    describe('Test #7 "To determin if the program tells user that randomly generated number is between 0 and 99 ', () => {
+      var userInput = 105
+      it('Should print "Randomly generated number is between 0 and 99" ', () => {
+        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('Randomly generated number is between 0 and 99')
+      })
+    })
+    // Test #8
+    describe('Test #8 "To determine if the program responses "Invalid Try, please put integer value" ', () => {
+      var userInput = 'abcd'
+      it('Should print "Invalid Try, please put integer value in to guess" ', () => {
+        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('Invalid Try, please put integer value in to guess')
+      })
     })
   })
 })
